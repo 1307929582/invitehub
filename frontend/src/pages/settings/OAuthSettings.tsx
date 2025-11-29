477354c8c6dbe68f68cfc30aae6f81f5d887ab22
xmdbd <cg1307929582@gmail.com>
2025-11-29 22:33:08 +0800

    fix: 移除未使用的 Space import

--- frontend/src/pages/settings/OAuthSettings.tsx
+++ frontend/src/pages/settings/OAuthSettings.tsx
@@ -1,6 +1,6 @@
 import { useEffect, useState } from 'react'
 import { useNavigate } from 'react-router-dom'
-import { Card, Form, Input, Button, message, Alert, Space } from 'antd'
+import { Card, Form, Input, Button, message, Alert } from 'antd'
 import { SaveOutlined, ArrowLeftOutlined, SafetyOutlined } from '@ant-design/icons'
 import { configApi } from '../../api'
 
