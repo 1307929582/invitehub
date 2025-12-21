52b3681f53e775a43bdecfd3b750144c1d4116b1
xmdbd <cg1307929582@gmail.com>
2025-12-21 11:42:06 +0800

    fix: 修正迁移文件 revision ID 格式

--- backend/alembic/versions/021_expand_ip_address_length.py
+++ backend/alembic/versions/021_expand_ip_address_length.py
@@ -1,14 +1,14 @@
 """expand ip_address length to 255
 
-Revision ID: 021
-Revises: 020
+Revision ID: 021_expand_ip_address_length
+Revises: 020_add_invite_status_reserved
 Create Date: 2025-12-21
 """
 from alembic import op
 import sqlalchemy as sa
 
-revision = '021'
-down_revision = '020'
+revision = '021_expand_ip_address_length'
+down_revision = '020_add_invite_status_reserved'
 branch_labels = None
 depends_on = None
 
